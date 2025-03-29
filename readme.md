b85456712e2a545c7e751e5520a92a5777542673
cliff_wang <codingcliff@pm.me>
2025-03-28 23:33:35 -0400

    add more testcases

--- readme.md
+++ readme.md
@@ -26,3 +26,24 @@ This is a demo to test https://github.com/aswinsmenon/currency-converter
 ```bash
 pytest
 ```
+
+## Tests Explaination
+### UI Tests
+1.  **Testcase 1:**
+    
+    Check if curreny converted as expected
+    * 1 aave = 238.34 ada
+    * 1 aave = 664.25 aed
+    * 1 aave = 1 aave
+    > Note:
+    >   We can edit *data/convert_test.csv* file to add more testcases
+2.  **Testcase 2:**
+    
+    Check if updating input will change converted value correctly
+    * 1 ada = 2.82 aed
+    * 2 ada = 5.64 aed
+    * 10000000000 ada = 28176809869.39 aed
+    > Note:
+    >   We can edit *data/convert_test.csv* file to add more testcases
+
+### API Tests (tbd)
--- test/test_ui.py
+++ test/test_ui.py
@@ -6,19 +6,19 @@ from pathlib import Path
 
 TEST_DATA_CSV = Path('data/convert_test.csv')
 try:
-    test_data = load_csv(TEST_DATA_CSV, ["from_cur", "to_cur", "expected"])
+    test_data = load_csv(TEST_DATA_CSV, ["input","from_cur", "to_cur", "expected"])
 except Exception as e:
     test_data = []
-    pytest.skip(f"Skipping due to loading {TEST_DATA_CSV}: {e}", allow_module_level=True)
+    pytest.skip(f"Skipping due to loading csv files: {e}", allow_module_level=True)
 
 @pytest.mark.parametrize(
-        "from_cur, to_cur, expected",
+        "input, from_cur, to_cur, expected",
         test_data
 )
-def test_result_appear(page, from_cur, to_cur, expected):
+def test_result_appear(page, input, from_cur, to_cur, expected):
     hompage = HomePage(page)
     hompage.get()
-    hompage.fill_input("1")
+    hompage.fill_input(input)
     hompage.select(from_cur, to_cur)
     hompage.click_convert()
     hompage.has_result()
